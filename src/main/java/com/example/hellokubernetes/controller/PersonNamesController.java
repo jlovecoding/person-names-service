64ce7e3f7880ec22fa994a45e9326552c8c0113c
jlovecoding <jlovecoding@gmail.com>
2019-03-11 21:59:20 +0100

    change deployment to run two pods and verify the balancing between both including pod local ip in returned response

--- src/main/java/com/example/hellokubernetes/controller/PersonNamesController.java
+++ src/main/java/com/example/hellokubernetes/controller/PersonNamesController.java
@@ -1,8 +1,13 @@
 package com.example.hellokubernetes.controller;
 
-import org.apache.commons.lang.RandomStringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.UnknownHostException;
+
+import static java.lang.String.format;
+import static java.net.InetAddress.getLocalHost;
+import static org.apache.commons.lang.RandomStringUtils.random;
 
 @RestController
 public class PersonNamesController {
@@ -12,6 +17,17 @@ public class PersonNamesController {
         int length = 10;
         boolean useLetters = true;
         boolean useNumbers = false;
-        return RandomStringUtils.random(length, useLetters, useNumbers);
+        return format("from-%s-%s", getLocalIp(), random(length, useLetters, useNumbers));
+    }
+
+    private String getLocalIp() {
+        String localIp = "unknown";
+        try {
+            localIp = getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+
+        return localIp;
     }
 }
